c4a22342c5103f7823ee8cf0743a33a27bafb68f
CaptainProton42 <john.wigg@gmx.net>
2020-04-20 22:46:29 +0200

    Fixed crash on html.

--- scripts/GoalBox.gd
+++ scripts/GoalBox.gd
@@ -3,5 +3,9 @@ extends StaticBody
 onready var animation_player = get_node("AnimationPlayer")
 onready var win_trigger = get_node("WinTrigger")
 
+func disable_lid_collision():
+	get_node("Lid/CollisionShape").disabled = true
+
 func _ready():
 	win_trigger.connect("boxed", animation_player, "play", ["close"])
+	win_trigger.connect("boxed", self, "disable_lid_collision")
